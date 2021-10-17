7ea7eb0f2014a5c612fe71184e12bdd9adc770ae
GTkatya <ekaterinaguryanova66@gmail.com>
2021-10-18 02:34:51 +0700

    First project on Android Studio

---
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "ya Sosiska!", Toast.LENGTH_SHORT);
+                toast.show();
+                // Code here executes on main thread after user presses button
+            }
+        });
+    }
+}
